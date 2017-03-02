88ea422a8e9d99b10c91365d4a393a4424a7d8fc
Dess1e <puditi@i.ua>
2017-03-02 23:49:17 +0200

    backup

--- flaskchat/main.py
+++ flaskchat/main.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, redirect
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config.from_object('config')
-#x
 
-class LoginForm(Form):
+
+class LoginForm(FlaskForm):
     openid = StringField('openid', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
 
@@ -22,6 +22,12 @@ def index():
     user_ = {'nickname': 'andriy'}
     lst = ["mem", "xyu", "bib"]
     return render_template("index.html", title="root", user=user_, memeslist=lst)
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
